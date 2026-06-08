e810829c7ff5025d9372b0c107b11de82adde67a
NashmanAidros711 <164944653+NashmanAidros711@users.noreply.github.com>
2026-06-08 16:36:45 +0300

    Change host URL in data.js
    Updated the host URL from Telegram to Google.

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-module.exports = {
-  "token": "7806905767:AAFz0p0pGSJWHcC9GA4t8Z4BB1twv46-Gc8",
-  "id": "1002670694",
-  "host": "https://t.me/Y_20_20"
+{
+    "token": "7806905767:AAFz0p0pGSJWHcC9GA4t8Z4BB1twv46-Gc8",
+    "id": "1002670694",
+    "host": "https://google.com/"
 }
